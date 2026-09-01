cd243afd8c414dc4cd723efaa8487542a09877e8
Kaan K <kaankeskindil06@gmail.com>
2026-09-01 17:08:50 +0000

    timestamp microservice

--- build-a-timestamp-microservice/server.js
+++ build-a-timestamp-microservice/server.js
@@ -13,6 +13,31 @@ app.get("/", (_req, res) => {
 
 // Do not change code above this line
 
+app.get("/api{/:date}", (req, res) => {let { date } = req.params; let dateObj;
+
+  if (!date) {
+    dateObj = new Date();
+  } else if (/^\d+$/.test(date)) {
+    dateObj = new Date(parseInt(date));
+  } else {
+    dateObj = new Date(date);
+  }
+
+if (dateObj.toString() === "Invalid Date") {
+  return res.json({ error: "Invalid Date"});
+}
+
+res.json({
+  unix: dateObj.getTime(),
+  utc: dateObj.toUTCString(),
+  });
+
+});
+
+
+
+
+
 // Do not change code below this line
 
 const PORT = 8000;
